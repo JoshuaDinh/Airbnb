4e3b95171df29015b6f1474f1b8ae899c2c7d19d
Joshua Dinh <joshuahungdinh@gmail.com>
2021-06-11 14:25:53 -0700

    added files

---
+++ src/Components/SearchDates/SearchDates.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./searchDates.css";
+const SearchDates = () => {
+  return <div className="searchDates">sd</div>;
+};
+
+export default SearchDates;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Components/Header/Header";
+import Banner from "../Components/Banner/Banner";
 import "./home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <Header />
+      <Banner />
     </div>
   );
 };
