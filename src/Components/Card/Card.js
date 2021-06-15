d533efe204fba45c36d04b9bd1d5e829856b4919
Joshua Dinh <joshuahungdinh@gmail.com>
2021-06-15 13:39:45 -0700

    added html layout

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./card.css";
 
-const Card = () => {
-  return <div className="card">card</div>;
+const Card = ({ src, title, desctiption, price }) => {
+  return (
+    <div className="card">
+      <img src={src} alt="#" className="card-img" />{" "}
+      <div className="card-info"></div>
+    </div>
+  );
 };
 
 export default Card;
