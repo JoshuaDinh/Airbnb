974dc0ce2985f76920316a5ab4ef19687a877104
Joshua Dinh <joshuahungdinh@gmail.com>
2021-06-15 14:16:32 -0700

    updated layout styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Home from "./Home/Home";
+import Header from "./Components/Header/Header";
 
 const App = () => {
   return (
     <div className="app">
+      <Header />
       <Home />
     </div>
   );
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,21 +3,53 @@ import Header from "../Components/Header/Header";
 import Banner from "../Components/Banner/Banner";
 import Card from "../Components/Card/Card";
 import "./home.css";
+import roomOne from "../images/room-1.jpg";
+import roomTwo from "../images/room-2.jpg";
+import roomThree from "../images/room-3.jpg";
+import houseOne from "../images/house-1.jpg";
+import houseTwo from "../images/house-2.jpg";
+import houseThree from "../images/house-3.jpg";
 
 const Home = () => {
   return (
     <div className="home">
-      <Header />
       <Banner />
       <div className="home-section">
-        <Card src="" title="" description="" />
-        <Card src="" title="" description="" />
-        <Card src="" title="" description="" />
+        <Card
+          src={houseOne}
+          title="Online Experiences"
+          description="Unique activities we can do together, led by a world of hosts"
+        />
+        <Card
+          src={houseTwo}
+          title="Entire Homes"
+          description="Comfortable private places, with room for friends or family"
+        />
+        <Card
+          src={houseThree}
+          title="Unique Stays"
+          description="Spaces that are more that just a place to sleep"
+        />
       </div>
       <div className="home-section">
-        <Card src="" title="" description="" />
-        <Card src="" title="" description="" />
-        <Card src="" title="" description="" />
+        <Card
+          src={roomOne}
+          title="Studio"
+          description="Enjoy amazing sites downtown in the heart of San Diego"
+          price="$157/night"
+        />
+        <Card
+          src={roomTwo}
+          title="1 Bedroom apartment"
+          description="Cozy and quiant, perfect home getaway"
+          price="$190/night"
+        />
+        <Card
+          src={roomThree}
+          title="2 Bedroom House"
+          description="Superhost with great amenities and fabulous shopping center nearby"
+          price="$220/night"
+        />
       </div>
     </div>
   );
