6a6787cdbe2a90418f698c42b89719092d0c7139
Joshua Dinh <joshuahungdinh@gmail.com>
2021-06-15 13:39:08 -0700

    added props to card

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../Components/Header/Header";
 import Banner from "../Components/Banner/Banner";
+import Card from "../Components/Card/Card";
 import "./home.css";
 
 const Home = () => {
@@ -8,6 +9,16 @@ const Home = () => {
     <div className="home">
       <Header />
       <Banner />
+      <div className="home-section">
+        <Card src="" title="" description="" />
+        <Card src="" title="" description="" />
+        <Card src="" title="" description="" />
+      </div>
+      <div className="home-section">
+        <Card src="" title="" description="" />
+        <Card src="" title="" description="" />
+        <Card src="" title="" description="" />
+      </div>
     </div>
   );
 };
