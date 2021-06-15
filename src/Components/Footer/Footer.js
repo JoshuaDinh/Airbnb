f05d8d7cd10687109da30cf35c6015bb83a564a9
Joshua Dinh <joshuahungdinh@gmail.com>
2021-06-15 16:26:42 -0700

    added footer html & css

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./footer.css";
 
 const Footer = () => {
-  return <div className="footer">footer</div>;
+  return (
+    <div className="footer">
+      <p> &copy; 2020 Airbnb clone! Nor rights reserved - this is a demo</p>
+      <p>Privacy - Terms -Sitemap - Company Details</p>
+    </div>
+  );
 };
 
 export default Footer;
